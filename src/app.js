c68f0a85c844eb1658a37dafad0bc57697d3d00a
gaurav876409 <gaurav876409@gmail.com>
2023-07-16 12:28:30 +0530

    up

--- src/app.js
+++ src/app.js
@@ -11,6 +11,42 @@ const userDetails = JSON.parse(
 app.use(express.json());
 
 // Write PATCH endpoint for editing user details
+app.patch("/api/v1/details/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const updatedDetails = userDetails.find(
+    (updatedDetails) => updatedDetails.id === id
+  );
+  const index = userDetails.indexOf(updatedDetails);
+  if (!updatedDetails) {
+    return res.status(404).send({
+      status: "failed",
+      message: "User not found!",
+    });
+  }
+
+  Object.assign(updatedDetails, req.body);
+
+  fs.writeFile(
+    `${__dirname}/data/userDetails.json`,
+    JSON.stringify(userDetails),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: "failed",
+          message: "An error occurred while updating the user details.",
+        });
+      }
+      res.status(200).json({
+        status: "success",
+        message: `User details updated successfully for id: ${updatedDetails.id}`,
+        data: {
+          updatedDetails,
+        },
+      });
+    }
+  );
+});
+
 
 // POST endpoint for registering new user
 app.post("/api/v1/details", (req, res) => {
